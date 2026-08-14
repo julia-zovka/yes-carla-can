1a942578988bd53910216e0805e8306962adccf6
mzckawa <5689.melzion.cmr@gmail.com>
2026-08-14 12:21:19 -0300

    Update actuator.py
    Now the model sends the messages directly to the CAN network, instead of emulating a physical keyboard input (which is unnecessary for our research purposes)

--- ai-pipeline/actuator.py
+++ ai-pipeline/actuator.py
@@ -1,18 +1,24 @@
-from pynput.keyboard import Key, Controller
+from can_network.network import CAN_Network 
+import carla
 
-keyboard = Controller()
+can_net = CAN_Network()
 manual_override_blocked = False
 
 def execute_control(command):
     global manual_override_blocked
     
+    control = carla.VehicleControl()
+
     if command == "FORWARD" and not manual_override_blocked:
-        keyboard.press('w')
+        control.throttle = 0.7  # 50% de acelerador
+        control.brake = 0.0
     elif command == "STOP":
-        keyboard.release('w')
-        # Trava tentativas manuais enquanto houver risco
+        control.throttle = 0.0
+        control.brake = 1.0     # 80% de freio
         manual_override_blocked = True 
 
-    # Desbloqueia quando a pista estiver livre novamente
     if command == "FORWARD":
         manual_override_blocked = False
+
+    # Envia o comando estruturado via barramento CAN para o simulador
+    can_net.send_msg(control)
