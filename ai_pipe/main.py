50dac8fff49b9c5a36c98a49e857e0250fc718bd
mzckawa <5689.melzion.cmr@gmail.com>
2026-09-25 15:24:36 -0300

    Mock de ia testado - funcao chamada dentro da classe RGBCameraSensor, no método estático _on_image

--- ai_pipe/main.py
+++ ai_pipe/main.py
@@ -1,5 +1,7 @@
 import sys 
 from pathlib import Path
+import carla
+from queue import LifoQueue
 
 # encontrando o caminho dos imports em relação ao diretório do repositório 
 curr_folder_abs_path = Path(__file__).resolve() # encontrando o caminho absoluto da pasta atual
@@ -11,14 +13,16 @@ while curr_folder_abs_path.name != "yes-carla-can" and curr_folder_abs_path.pare
 # se saímos do while, é porque encontramos o caminho correto 
 sys.path.insert(0, str(curr_folder_abs_path)) # colocando encontrado como o primeiro na fila de busca por módulos e arquivos
 
-# instanciando a câmera (versão sem AVTP)
-from sensors import camera as camera
+from sensors.camera import RGBCameraSensor
+from gui.world import World
 
-camera = camera.RGBCameraSensor("AutonomousEyes");
-camera.recording = True
+# criando a pilha que guardará as imagens para serem processadas
+ai_queue = LifoQueue()
+camera = World.rgb_camera_sensor
 
-<<<<<<< HEAD
-print(f"{camera.RGBCameraSensor.recording}")
-=======
-print(f"{camera.RGBCameraSensor.recording}")
->>>>>>> aba5df1356824faccc348b2ba6e633ec4fd3cdb8
+def ai_callback(image, queue):
+    
+
+
+
+
--- sensors/camera.py
+++ sensors/camera.py
@@ -6,7 +6,54 @@ import weakref
 import numpy as np
 import pygame
 import carla
+import time
+from random import randint 
 
+def prosseguir_ou_frear(imagem_rgb):
+
+    # verificando se a imagem recebida está no formato correto (tridimensional) 
+    if imagem_rgb.shape[2] != 3:
+        print(f"Formato inválido. Imagem com {imagem_rgb.shape} dimensões.\n")
+        return 
+    
+    altura, largura, _ = imagem_rgb.shape
+
+    # definindo um polígono como nossa região de interesse (quadrado logo à frente do carro, no centro inferior)
+    roi = imagem_rgb[int(altura*0.7):int(altura*0.9), int(altura*0.4):int(largura*0.6)]
+    
+    # convertendo para esscala de cinza de forma simples
+    gray_roi = np.mean(roi, axis=2)
+
+    # contando pixels que estão na faixa de cor do asfalto (dentro da escala de cinza)
+    pixels_pista = np.sum((gray_roi > 60) & (gray_roi < 120)) # valores estimados
+    total_pixels = gray_roi.size 
+    proporcao_pista = pixels_pista / total_pixels
+
+    # gerando ruído, para simular oscilações no valor final gerado por um modelo mais robusto
+    random1 = randint(10, 50)
+    random2 = randint(10, 50)
+    ruido1 = random1/100
+    ruido2 = random2/100
+
+    # se mais de 40% da área for de pixels na escala do asfalto, consideramos que o caminho está livre, logo, o carro será acelerado
+    if proporcao_pista > 0.4:
+
+        throttle = 0.3 - ruido1
+        brake = 0.0 + ruido2
+
+
+    # do contrário, o carro deve ser freado
+    else: 
+
+        throttle = 0.0 + ruido1
+        brake = 1.0 - ruido2
+
+    # simulando uma "demora" de inferência, que deve acontecer quando colocarmos um modelo de verdade para funcionar
+    time.sleep(0.03)
+
+    print(f"Array inicial: {imagem_rgb}; throttle: {throttle}, brake: {brake}.\n")
+
+    return throttle, brake
 
 class RGBCameraSensor(object):
     """
@@ -112,6 +159,9 @@ class RGBCameraSensor(object):
 
         # Store numpy array (RGB, uint8) — use this for data export.
         self.array = array
+
+        prosseguir_ou_frear(array)
+
 
         # Build pygame surface (expects (W, H) axis order).
         self.surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
